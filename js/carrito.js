9114d731d355c805b980a12fcb584216c3269249
¨lucia_ab¨ <luabramzon@gmail.com>
2021-12-06 17:33:15 -0300

    comentarios

--- js/carrito.js
+++ js/carrito.js
@@ -1,164 +1,88 @@
-$(()=>{
-infoCarrito=localStorage.getItem("carrito")
-infoCarrito=JSON.parse(infoCarrito)
 
-$("#cupon").append(`
-    <input type=text id=cuponTipo>
-    <button id=btnDescuento> Aplicar cupon </button>
-`)
-let precioTotal=0
-let iva=precioTotal*0.21
-let descuento=0
 
-mostrarCarrito()
-function mostrarCarrito(){ 
-let carritoHTML="<p>";
-    for (const id in infoCarrito){ 
-        let producto=infoCarrito[id]
-        carritoHTML+=
-         `<div class="carritoProductos">
-        <img src=.${producto.imagen} height=150px>
-        <h5> ${producto.nombre}</h5> 
-        <h5> Talle: ${producto.talle}</h5>
-        <h6 > Precio: $ ${producto.precio}</h6>
-        <button id="carrito_${id}"  class="borrar"> Eliminar </button>
-        </div>`
-        carritoHTML+="<p>"
-        $("#productosCarrito").html(carritoHTML)
-        precioTotal+=producto.precio; 
-    }
-
-let descuento= 0
-function descuentoCupon(){ 
-        $("#cuponTipo").change((e)=>{
-        if(e.target.value=="fiestas"){
-        descuento=100
-        $("#descuento").html(`descuento $ ${-descuento}`)
-        }else if(e.target.value=="original"){
-        descuento=400
-        $("#descuento").html(`descuento $ ${-descuento}`)
-        }else{
-        descuento=0
-        $("#descuento").html(`descuento $ ${-descuento}`)
-        }
-        })
-            $("#btnDescuento").click(()=>{ descuentoCupon()
-                 total()})
-        
-        } 
-
-let iva=precioTotal*0.21
-function precio(){ 
-        $("#total").html(`Subtotal: $ ${precioTotal}`);
-        $("#iva").html (`Iva: $ ${iva}`)
-    }
-
-function total(){
-        $("#carritoTotal").html(`Total a pagar: $ ${precioTotal+iva-descuento}`)
-    }
-
-precio()
-btnEliminar()
-descuentoCupon()
-total()
-
-}
-
-function btnEliminar() {  
-    let borrar=document.getElementsByClassName("borrar")
-    for  (const btn of borrar){
-        btn.onclick=()=>{ 
-        let id=btn.getAttribute("id");
-        idNumber=id.split("_")[1];
-        localStorage.removeItem(carrito.splice(idNumber,1)) 
-        console.log(carrito)
-    
-       }
-        }
-  
-} 
-
-/* $(()=>{
+ $(()=>{
     infoCarrito=localStorage.getItem("carrito")
     infoCarrito=JSON.parse(infoCarrito)
-    
-    
-    let precioTotal=0
-    
-    mostrarCarrito()
-    function mostrarCarrito(){ 
-    let carritoHTML="<p>";
-    for (const id in infoCarrito){ 
-        let producto=infoCarrito[id]
-        carritoHTML+=
-             `<div class="carritoProductos">
-            <img src=.${producto.imagen} height=150px>
-            <h5> ${producto.nombre}</h5> 
-            <h5> Talle: ${producto.talle}</h5>
-            <h6 > Precio: $ ${producto.precio}</h6>
-            <button id="carrito_${id}"  class="borrar"> Eliminar </button>
-        </div>`
-        carritoHTML+="<p>"
-    
-        $("#productosCarrito").html(carritoHTML)
-        precioTotal+=producto.precio;
-    
-    }
-    
-    let iva=precioTotal*0.21
-    $("#total").html(`Subtotal: $ ${precioTotal}`)
-    $("#iva").html (`Iva: $ ${iva}`)
-    
     
     $("#cupon").append(`
         <input type=text id=cuponTipo>
         <button id=btnDescuento> Aplicar cupon </button>
     `)
+    let precioTotal=0
+    let iva=precioTotal*0.21
+    let descuento=0
+    
+    mostrarCarrito()
+
+    function mostrarCarrito(){ 
+    let carritoHTML="<p>";
+        for (const id in infoCarrito){ 
+            let producto=infoCarrito[id]
+            carritoHTML+=
+             `<div id="carritoProductos" class="carritoProductos_${id}">
+            <img src=.${producto.imagen} height=150px>
+            <h5> ${producto.nombre}</h5> 
+            <h5> Talle: ${producto.talle}</h5>
+            <h6 > Precio: $ ${producto.precio}</h6>
+            <button id="carrito_${id}"  class="borrar"> Eliminar </button>
+            </div>`
+            carritoHTML+="<p>"
+            $("#productosCarrito").html(carritoHTML)
+            precioTotal+=producto.precio; 
+        }
     
     let descuento= 0
     function descuentoCupon(){ 
-    $("#cuponTipo").change((e)=>{
-        if(e.target.value=="fiestas"){
+            $("#cuponTipo").change((e)=>{
+            if(e.target.value=="fiestas"){
             descuento=100
             $("#descuento").html(`descuento $ ${-descuento}`)
-        }else if(e.target.value=="original"){
+            }else if(e.target.value=="original"){
             descuento=400
             $("#descuento").html(`descuento $ ${-descuento}`)
-        }else{
+            }else{
             descuento=0
             $("#descuento").html(`descuento $ ${-descuento}`)
+            }
+            })
+                $("#btnDescuento").click(()=>{ descuentoCupon()
+                     total()})
+            
+            } 
+    
+    let iva=precioTotal*0.21
+    function precio(){ 
+            $("#total").html(`Subtotal: $ ${precioTotal}`);
+            $("#iva").html (`Iva: $ ${iva}`)
         }
-        carritoTotal()
-    })
-    } 
-    $("#btnDescuento").click(()=>{ 
-        descuentoCupon()})
-        function carritoTotal(){ 
-    $("#carritoTotal").html(`Total a pagar: $ ${precioTotal+iva-descuento}`)}
+    
+    function total(){
+            $("#carritoTotal").html(`Total a pagar: $ ${precioTotal+iva-descuento}`)
+        }
+    
+    precio()
     btnEliminar()
+    descuentoCupon()
+    total()
+    
     }
-    
-    
-    
     
     function btnEliminar() {  
         let borrar=document.getElementsByClassName("borrar")
         for  (const btn of borrar){
             btn.onclick=()=>{ 
+            localStorage.getItem("carrito")
             let id=btn.getAttribute("id");
             idNumber=id.split("_")[1];
-           let eliminado= carrito.splice(idNumber,1) 
-            localStorage.removeItem(eliminado)
-            console.log(eliminado)
-    
-        
-           }
+            localStorage.removeItem("carrito",carrito.splice(idNumber,1))
+            let carrito2=JSON.stringify(carrito)
+            localStorage.setItem("carrito",carrito2)
+            $(`.carritoProductos_${idNumber}`).remove()
+            location.reload()
+        }   
             }
       
     } 
-    
-    
-    
     
     $("#comprar").click(()=>{
         alert("Su compra esta siendo procesada")
@@ -168,24 +92,7 @@ function btnEliminar() {
         document.getElementById("iva").innerHTML=" "
         document.getElementById("cupon").innerHTML=" "
         document.getElementById("descuento").innerHTML=" "
+        document.getElementById("carritoTotal").innerHTML=" "
     })
     
-    }) */
-
-
-
-
-$("#comprar").click(()=>{
-    alert("Su compra esta siendo procesada")
-    localStorage.removeItem("carrito")
-    document.getElementById("productosCarrito").innerHTML=" "
-    document.getElementById("total").innerHTML=" "
-    document.getElementById("iva").innerHTML=" "
-    document.getElementById("cupon").innerHTML=" "
-    document.getElementById("descuento").innerHTML=" "
-    document.getElementById("carritoTotal").innerHTML=" "
-})
-
-})
-
-
+    }) 
--- js/imgHTML.js
+++ js/imgHTML.js
@@ -1,71 +1,67 @@
+let infoCarrito=localStorage.getItem("carrito")
+let carrito = [];
 
-
-   
-   let infoCarrito=localStorage.getItem("carrito")
-   let carrito = [];
-   
-    if(infoCarrito)
-    carrito=JSON.parse(infoCarrito)
+ if(infoCarrito)
+ carrito=JSON.parse(infoCarrito)
 
 
 
-    function carritoNumero(){
-        $("#qCarrito").html(carrito.length)
-    }
+ function carritoNumero(){
+     $("#qCarrito").html(carrito.length)
+ }
 
-    function guardarCarrito(compra){
-    carrito.push(compra)
-    let carritoString=JSON.stringify(carrito);
-    localStorage.setItem("carrito",carritoString)
+ function guardarCarrito(compra){
+ carrito.push(compra)
+ let carritoString=JSON.stringify(carrito);
+ localStorage.setItem("carrito",carritoString)
 
-    carritoNumero()}
+ carritoNumero()}
 
-    function guardarCarrito2(){
-                let carritoString=JSON.stringify(carrito);
-        localStorage.setItem("carrito",carritoString)
+ function guardarCarrito2(){
+             let carritoString=JSON.stringify(carrito);
+     localStorage.setItem("carrito",carritoString)
+ 
+     carritoNumero()}
+
+ $(()=> {
+     carritoNumero()
+ for (const vestido of vestidos){ 
+     $("#productosVestidos").append(
+         `<div>
+         <img src=${vestido.imagen} height=200px>
+         <h5> Producto: ${vestido.nombre}</h5> 
+         <h5> Talle: ${vestido.talle}
+         <h6 > Precio: $ ${vestido.precio}</h6>
+         <button id="producto${vestido.id}" > Lo quiero </button>
+     </div>`
+     )
     
-        carritoNumero()}
+      $(`#producto${vestido.id}`).click(()=>guardarCarrito(vestido)) 
 
-    $(()=> {
-        carritoNumero()
-    for (const vestido of vestidos){ 
-        $("#productosVestidos").append(
-            `<div>
-            <img src=${vestido.imagen} height=200px>
-            <h5> Producto: ${vestido.nombre}</h5> 
-            <h5> Talle: ${vestido.talle}
-            <h6 > Precio: $ ${vestido.precio}</h6>
-            <button id="producto${vestido.id}" > Lo quiero </button>
-        </div>`
-        )
-       
-         $(`#producto${vestido.id}`).click(()=>guardarCarrito(vestido)) 
-  
-        
-    }
-    
-    for (const zapato of zapatos){
-        $("#productosZapatos").append(
-            `<div> <img src=${zapato.imagen} height=200px>
-            <h5> Producto: ${zapato.nombre}</h5> 
-            <h5> Talle: ${zapato.talle}
-            <h6> Precio: $ ${zapato.precio}</h6>
-            <button id="producto${zapato.id}" > Lo quiero </button></div>`
-        )
-        $(`#producto${zapato.id}`).click(()=>guardarCarrito(zapato))
-    }
-    
-    for (const accesorio of accesorios){
-        $("#productosAccesorios").append(
-            `<div>
-            <img src=${accesorio.imagen} height=200px>
-            <h5> Producto: ${accesorio.nombre}</h5> 
-            <h5> Talle: ${accesorio.talle}
-            <h6> Precio: $ ${accesorio.precio}</h6>
-            <button id="producto${accesorio.id}" > Lo quiero </button> </div>`
-        )
-        $(`#producto${accesorio.id}`).click(()=>guardarCarrito(accesorio))
-    }
-    
+     
+ }
+ 
+ for (const zapato of zapatos){
+     $("#productosZapatos").append(
+         `<div> <img src=${zapato.imagen} height=200px>
+         <h5> Producto: ${zapato.nombre}</h5> 
+         <h5> Talle: ${zapato.talle}
+         <h6> Precio: $ ${zapato.precio}</h6>
+         <button id="producto${zapato.id}" > Lo quiero </button></div>`
+     )
+     $(`#producto${zapato.id}`).click(()=>guardarCarrito(zapato))
+ }
+ 
+ for (const accesorio of accesorios){
+     $("#productosAccesorios").append(
+         `<div>
+         <img src=${accesorio.imagen} height=200px>
+         <h5> Producto: ${accesorio.nombre}</h5> 
+         <h5> Talle: ${accesorio.talle}
+         <h6> Precio: $ ${accesorio.precio}</h6>
+         <button id="producto${accesorio.id}" > Lo quiero </button> </div>`
+     )
+     $(`#producto${accesorio.id}`).click(()=>guardarCarrito(accesorio))
+ }
+ 
 })
-
