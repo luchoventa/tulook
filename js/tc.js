47f60ebbf6cc3cc7baf82da2056f1661ca030ead
¨lucia_ab¨ <luabramzon@gmail.com>
2021-12-21 16:36:04 -0300

    api

---
+++ js/tc.js
@@ -0,0 +1,44 @@
+const monedaUno = document.getElementById('moneda-uno');
+const monedaDos = document.getElementById('moneda-dos');
+const cantMUno = document.getElementById('cantidad-uno');
+const cantMDos = document.getElementById('cantidad-dos');
+const cambio = document.getElementById('cambio');
+const taza1 = document.getElementById('taza');
+
+$("#tcBtn").click(()=>{
+    $("#tc").toggle("slow")
+})
+
+// Fetch Exchange Rate and Update the DOM
+function calculate(){
+    const mUno = monedaUno.value;
+    const mDos = monedaDos.value;
+
+   fetch(`https://api.exchangerate-api.com/v4/latest/${mUno}`)
+   .then(res => res.json() )
+   .then(data => {
+       const taza = data.rates[mDos];
+       
+       cambio.innerText = `1 ${mUno} = ${taza} ${mDos}`;
+
+       cantMDos.value = (cantMUno.value * taza).toFixed(2);
+
+    } );
+    
+}
+
+//Event listeners
+monedaUno.addEventListener('change', calculate);
+cantMUno.addEventListener('input', calculate);
+monedaDos.addEventListener('change', calculate);
+cantMDos.addEventListener('input', calculate);
+
+taza.addEventListener('click', () =>{
+    const temp = monedaUno.value;
+    monedaUno.value = monedaDos.value;
+    monedaDos.value = temp;
+    calculate();
+} );
+
+
+calculate();
